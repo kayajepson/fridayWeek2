dccd1ae364babf33774cbd18e998b503c829bc1b
Kaya Jepson <jepsonkaya@gmail.com>
2019-03-29 19:42:38 +0000

    add custom pages for each track

--- js/scripts.js
+++ js/scripts.js
@@ -10,38 +10,53 @@ $(document).ready(function() {
     if (interest === "Front-End") {
       console.log("front");
       // $("#deck1").show();
-      window.location.href = "test.html"
+      window.location.href = "cssDesign.html"
 
     } else {
           if (environment === "a CMS like Wordpress or Joomla" && time === "many" && size === "small") {
-          $("#deck2").show();
+          window.location.href = "phpDrupal.html"
           console.log("php");
           } else if (environment === "a CMS like Wordpress or Joomla" && time === "many" && size === "large") {
-          $("#deck2").show();
+          // $("#deck2").show();
+          window.location.href = "phpDrupal.html"
           console.log("php");
           } else if (environment === "a CMS like Wordpress or Joomla" && time === "few" && size === "large") {
-            $("#deck2").show();
+            // $("#deck2").show();
+            window.location.href = "phpDrupal.html"
             console.log("php");
           } else if (environment === "a CMS like Wordpress or Joomla" && time === "few" && size === "small") {
-              $("#deck2").show();
+              // $("#deck2").show();
+              window.location.href = "phpDrupal.html"
               console.log("php");
           } else if (environment === "Android" && time === "many" && size === "large") {
-          $("#deck3").show();
+            // $("#deck3").show();
+            window.location.href = "javaAndroid.html"
           console.log("java");
           } else if (environment === "Android" && time === "few" && size === "large") {
-            $("#deck3").show();
+            // $("#deck3").show();
+            window.location.href = "javaAndroid.html"
             console.log("java");
-          } else if (environment === "Android" && time === "few" && size === "small") {
-              $("#deck3").show();
+          } else if (environment === "Android" && time === "many" && size === "small") {
+              // $("#deck3").show();
+              window.location.href = "javaAndroid.html"
               console.log("java");
           } else if (environment === "Android" && time === "few" && size === "small") {
-              $("#deck3").show();
+              // $("#deck3").show();
+              window.location.href = "javaAndroid.html"
               console.log("java");
           } else if (interest === "Back-End" && time === "few" && size === "small") {
-            $("#deck3").show();
+            // $("#deck3").show();
+            window.location.href = "rubyRails.html"
+            console.log("ruby");
+          } else if (interest === "Back-End" && time === "many" && size === "small") {
+            // $("#deck3").show();
+            window.location.href = "rubyRails.html"
             console.log("ruby");
           } else if (interest === "Back-End" && time === "few" && size === "large") {
-            $("#deck5").show();
+            window.location.href = "cSharp.html"
+            console.log("csharp");
+          } else if (interest === "Back-End" && time === "many" && size === "large") {
+            window.location.href = "cSharp.html"
             console.log("csharp");
           } else {
             alert("Please fill out all questions.");
