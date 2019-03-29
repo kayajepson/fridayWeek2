df41619dd973c18d2a369a23e416d4a9fafb9609
Kaya Jepson <jepsonkaya@gmail.com>
2019-03-29 16:23:26 +0000

    add beginning bootstrap styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#User").submit(function(event) {
 
-    // var interest = $("input:radio[name=interest]:checked").val();
     var interest = $("#interest").val();
+    var environment = $("#environment").val();
     var time = $("input:radio[name=time]:checked").val();
     var size = $("input:radio[name=size]:checked").val();
 
@@ -10,24 +10,47 @@ $(document).ready(function() {
       console.log("front");
       $("#cssDesign").show();
     } else {
-
-        if (interest === "Back-End" && time === "many" && size === "small") {
+          if (environment === "a CMS like Wordpress or Joomla" && time === "many" && size === "small") {
           $("#php").show();
           console.log("php");
-        } else if (interest === "Back-End" && time === "many" && size === "large") {
+          } else if (environment === "a CMS like Wordpress or Joomla" && time === "many" && size === "large") {
+          $("#php").show();
+          console.log("php");
+          } else if (environment === "a CMS like Wordpress or Joomla" && time === "few" && size === "large") {
+            $("#php").show();
+            console.log("php");
+          } else if (environment === "a CMS like Wordpress or Joomla" && time === "few" && size === "small") {
+              $("#php").show();
+              console.log("php");
+          } else if (environment === "Android" && time === "many" && size === "large") {
           $("#java").show();
           console.log("java");
-        } else if (interest === "Back-End" && time === "few" && size === "small") {
-          $("#rubyRails").show();
-          console.log("ruby");
-        } else if (interest === "Back-End" && time === "few" && size === "large") {
-          $("#cSharp").show();
-          console.log("csharp");
-        } else {
-          alert("Please fill out all questions.");
-          console.log("error");
-          return;
-        }
+          } else if (environment === "Android" && time === "few" && size === "large") {
+            $("#java").show();
+            console.log("java");
+          } else if (environment === "Android" && time === "few" && size === "small") {
+              $("#java").show();
+              console.log("java");
+          } else if (environment === "Android" && time === "few" && size === "small") {
+              $("#java").show();
+              console.log("java");
+          } else if (interest === "Back-End" && time === "many" && size === "small") {
+            $("#php").show();
+            console.log("java");
+          } else if (interest === "Back-End" && time === "many" && size === "large") {
+            $("#java").show();
+            console.log("java");
+          } else if (interest === "Back-End" && time === "few" && size === "small") {
+            $("#rubyRails").show();
+            console.log("ruby");
+          } else if (interest === "Back-End" && time === "few" && size === "large") {
+            $("#cSharp").show();
+            console.log("csharp");
+          } else {
+            alert("Please fill out all questions.");
+            console.log("error");
+            return;
+      }
     }
     event.preventDefault();
   });
